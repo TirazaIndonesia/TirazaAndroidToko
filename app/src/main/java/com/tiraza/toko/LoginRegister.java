5803e048f05887a72d43bc76ee46c8e00242b7f5
Deef <deef.trz@gmail.com>
2017-05-08 11:59:36 +0700

    initial Commit

---
+++ app/src/main/java/com/tiraza/toko/LoginRegister.java
@@ -0,0 +1,13 @@
+package com.tiraza.toko;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class LoginRegister extends Activity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_login_register);
+    }
+}
---
+++ app/src/main/java/com/tiraza/toko/activity_register.java
@@ -0,0 +1,12 @@
+package com.tiraza.toko;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class activity_register extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+    }
+}
